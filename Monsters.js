1a098ad22284240c2538013dc2d9b37660bdea24
Martin S <martsve@gmail.com>
2018-02-16 14:55:03 +0100

    added templates for scenario

--- Monsters.js
+++ Monsters.js
@@ -1,15 +1,14 @@
 var MonsterCards = {
     "guard": {
-        1: { Image: "res/Guard/1.jpg", Initiative: 35 },
-        2: { Image: "res/Guard/2.jpg", Initiative: 55 },
-        3: { Image: "res/Guard/3.jpg", Initiative: 50 },
-        4: { Image: "res/Guard/4.jpg", Initiative: 50 },
-        5: { Image: "res/Guard/5.jpg", Initiative: 15 },
-        6: { Image: "res/Guard/6.jpg", Initiative: 15 },
-        7: { Image: "res/Guard/7.jpg", Initiative: 30 },
-        8: { Image: "res/Guard/8.jpg", Initiative: 70 },
+        1: { Image: "res/Guard/1.jpg", Initiative: 35, Move: -1, Attack: 0, Range: 2 },
+        2: { Image: "res/Guard/2.jpg", Initiative: 55, Move: -1, Attack: 0, Strengthen: true },
+        3: { Image: "res/Guard/3.jpg", Initiative: 50, Move: 0, Attack: 0 },
+        4: { Image: "res/Guard/4.jpg", Initiative: 50, Move: 0, Attack: 0 },
+        5: { Image: "res/Guard/5.jpg", Initiative: 15, Shield: 1, Attack: 0, Poison: true, Shuffle: true },
+        6: { Image: "res/Guard/6.jpg", Initiative: 15, Shield: 1, Retaliate: 2 },
+        7: { Image: "res/Guard/7.jpg", Initiative: 30, Move: 1, Attack: -1 },
+        8: { Image: "res/Guard/8.jpg", Initiative: 70, Move: -1, Attack: 1 },
     },
-
 };
 
 var Monsters = {
